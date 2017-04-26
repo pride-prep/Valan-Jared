d2a578db1d7a4fba9b1a2d6f27fa1f4751eb45fa
Obey-the-dankz <valan@my.prideprepschool.org>
2017-04-26 13:14:53 -0700

    Made a text field, button and a label
    boi

---
+++ Hello world checkpoint/Hello world checkpoint/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hello world checkpoint
+//
+//  Created by Student on 4/26/17.
+//  Copyright © 2017 C0de 4 Funn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var Firstlabel: UILabel!
+    @IBAction func Button1(_ sender: Any) {
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
