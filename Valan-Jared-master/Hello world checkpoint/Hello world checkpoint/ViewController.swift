26f4191b81c36fb0c2230d9024123fb4ffc1b67b
Obey-the-dankz <valan@my.prideprepschool.org>
2017-04-27 13:11:26 -0700

    added button to text field and it works m8.

---
+++ Valan-Jared-master/Hello world checkpoint/Hello world checkpoint/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Hello world checkpoint
+//
+//  Created by Student on 4/26/17.
+//  Copyright © 2017 C0de 4 Funn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var textfield1: UITextField!
+    @IBOutlet weak var Firstlabel: UILabel!
+    @IBAction func Button1(_ sender: Any) {
+        Firstlabel.text=textfield1.text
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
