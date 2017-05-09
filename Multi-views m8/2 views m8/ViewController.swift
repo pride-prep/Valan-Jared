2960e35ec35a7bebbc13fbecabc63de197b98cfd
Obey-the-dankz <valan@my.prideprepschool.org>
2017-05-09 13:10:07 -0700

    Add files via upload

---
+++ Multi-views m8/2 views m8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2 views m8
+//
+//  Created by Student on 5/9/17.
+//  Copyright © 2017 C0de 4 Funn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
