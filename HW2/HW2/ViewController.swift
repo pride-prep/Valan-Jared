8bab29d7790dd2f74ca4af4820adb1493fdd4541
Obey-the-dankz <valan@my.prideprepschool.org>
2017-05-10 13:10:40 -0700

    hi

--- HW2/HW2/ViewController.swift
+++ HW2/HW2/ViewController.swift
@@ -14,22 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var TASTY: UILabel!
+    @IBOutlet weak var TextField: UITextField!
     @IBAction func ChickenButton(_ sender: Any) {
     }
     @IBAction func SteakButton(_ sender: Any) {
     }
     @IBAction func XbossButton(_ sender: Any) {
-        func ComputerButton(_ sender: Any) {
+     
         }
+    @IBAction func ComputerCringe(_ sender: Any) {
+    }
         func TvButton(_ sender: Any) {
+        TASTY.text=TextField.text
         }
+        
     }
 
-
-}
 
