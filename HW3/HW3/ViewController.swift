48b18ebe273589a33aea006a4a7a66062f5c9f1c
Obey-the-dankz <valan@my.prideprepschool.org>
2017-05-11 13:11:21 -0700

    STARTED LIT QUIZ
    BOI

---
+++ HW3/HW3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW3
+//
+//  Created by Student on 5/11/17.
+//  Copyright © 2017 C0de 4 Funn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
