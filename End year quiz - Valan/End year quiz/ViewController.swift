7369e8282faa38aa9140cc7a5572b977eb109a84
Obey-the-dankz <valan@my.prideprepschool.org>
2017-06-07 12:21:00 -0700

    End year test m8

---
+++ End year quiz - Valan/End year quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  End year quiz
+//
+//  Created by Student on 5/30/17.
+//  Copyright © 2017 C0de 4 Funn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
